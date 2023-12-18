56e3335b951d21cc2013eacb3d3c7da8dffc57f4
philipwanjiku <muhorophilip7@gmail.com>
2023-12-18 13:18:37 +0300

    Instance variables for each customer

--- lib/main.py
+++ lib/main.py
@@ -3,3 +3,11 @@ from lib.Review import Review
 class Customer:
     # Class variable to store all instances of Customer
     all_customers = []
+    
+    def __init__(self, given_name, family_name):
+        # Instance variables for each customer
+        self.given_name = given_name
+        
+        
+    def given_name(self):
+        return self.given_name
