cecc8eb3d519304f06c84643e5c3790608f4f448
philipwanjiku <muhorophilip7@gmail.com>
2023-12-18 13:23:47 +0300

    class methods

--- lib/main.py
+++ lib/main.py
@@ -7,7 +7,11 @@ class Customer:
     def __init__(self, given_name, family_name):
         # Instance variables for each customer
         self.given_name = given_name
+        self.family_name = family_name
         
         
     def given_name(self):
         return self.given_name
+    
+    def family_name(self):
+        return self.family_name
