4aa3b0d129866ebde1e63657fd1e6da6af24eba4
philipwanjiku <muhorophilip7@gmail.com>
2023-12-18 13:29:35 +0300

    get name of restaurant

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -7,3 +7,7 @@ class Restaurant:
         self.name = name
         self.reviews = []  # List to store reviews associated with this restaurant
         Restaurant.all_restaurants.append(self)  # Add the instance to the class variable
+        
+    def get_name(self):
+        # Method to get the name of the restaurant
+        return self.name
