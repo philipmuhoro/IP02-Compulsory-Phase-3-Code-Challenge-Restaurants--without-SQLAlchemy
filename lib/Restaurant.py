c18ad75861674e86ef91179e4ca6f81ccde33fe7
philipwanjiku <muhorophilip7@gmail.com>
2023-12-18 13:28:23 +0300

    __init__ restaurant

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -2,4 +2,8 @@ class Restaurant:
     # Class variable to store all instances of Restaurant
     all_restaurants = []
     
-    
+    def __init__(self, name):
+        # Instance variables for each restaurant
+        self.name = name
+        self.reviews = []  # List to store reviews associated with this restaurant
+        Restaurant.all_restaurants.append(self)  # Add the instance to the class variable
